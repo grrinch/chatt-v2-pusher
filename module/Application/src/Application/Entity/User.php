bd61767c9cb252d801a0ffcbd284a58e06d29109
grrinch <radoslaw.paluszak@student.put.poznan.pl>
2013-12-09 21:00:36 +0100

    Integracja z Doctrine - Part 1 :)

---
+++ module/Application/src/Application/Entity/User.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Application\Entity;
+use Doctrine\ORM\Mapping as ORM;
+/** @ORM\Entity */
+class User {
+    /**
+    * @ORM\Id
+    * @ORM\GeneratedValue(strategy="AUTO")
+    * @ORM\Column(type="integer")
+    */
+    protected $id;
+
+    /** @ORM\Column(type="string") */
+    protected $fullName;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getFullName()
+    {
+        return $this->fullName;
+    }
+
+    public function setFullName($value)
+    {
+        $this->fullName = $value;
+    }
+}
